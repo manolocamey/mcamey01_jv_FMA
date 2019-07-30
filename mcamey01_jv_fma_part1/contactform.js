3c4febad776eacd45fca7d3248521dd20ca9fd13
manolocamey <manolocamey@gmail.com>
2019-07-30 23:04:02 +0100

    Changes to diabtes calculator and creation of js contact form with validation

--- mcamey01_jv_fma_part1/contactform.js
+++ mcamey01_jv_fma_part1/contactform.js
@@ -1,56 +1,135 @@
-// Getting values from contact form
-var fname = document.contact.fname.value;
-var lname = document.contact.lname.value;
-var title = document.contact.title.value;
-var han = document.contact.han.value;
-var email = document.contact.email.value;
-var phone = document.contact.phone.value;
-
-function bindings() {           
-    document.getElementById("submitBtn").onclick = displayResults;
-}
-
-function displayResults(){
-    inputFnameValue = validateFname(fname);
-    inputEmailValue = validateEmail(email);
-    inputPhoneValue = validatePhone(phone);
-}
-
-
-function validateFname(name){
-    var alphachar = /-[a-zA-Z]/;
-    if(name=="" && !name.match(alphachar) && isNaN(name)  ){
-        alert("Invalid Name format");
-    }
-}
-
-function validateEmail(mail) {
-    var emailVal = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (emailVal.test(mail)){
-        alert("Good!");
-        return (true)
-    }else{
-        alert("You have entered an invalid email address!")
-        return (false)
-    }
-}
-
-function validatePhone(tel){
-    var numericchar = /^[0-9]+$/;
-    var telSize = tel.length
-    if(name=="" && !name.match(numericchar) && isNaN(name)  ){
-        alert("Invalid Name format");
-    }
-}
-
 window.onload = bindings;
 
+function bindings() {
+    document.getElementById('submitBtn').onclick = submitForm;
+    document.getElementById('LastName').onfocus = clearError;
+    document.getElementById('LastName').onblur = checkLastName;
+    document.getElementById('Title').onfocus = clearError;
+    document.getElementById('Title').onblur = checkTitle;
+    document.getElementById('Han').onfocus = clearError;
+    document.getElementById('Han').onblur = checkHan;
+    document.getElementById('Email').onfocus = clearError;
+    document.getElementById('Email').onblur = checkEmail;
+    document.getElementById('Phone').onfocus = clearError;
+    document.getElementById('Phone').onblur = checkPhone;
+    checkFirstName();
+    toolTipDisplay();
+}
+
+function submitForm(){
+    var fnameCheck = checkFirstName();
+    var lnameCheck = checkLastName();
+    var titleCheck = checkTitle();
+    var hanCheck = checkHan();
+    var emailCheck = checkEmail();
+    var phoneCheck = checkPhone();
+
+	if(fnameCheck == false || lnameCheck == false || titleCheck == false || hanCheck == false || emailCheck == false || phoneCheck == false ){
+		document.getElementById('onclickError').innerHTML = " * There are errors in the form";
+		return false;
+    }
+}
+
+// Field Validation
+function checkFirstName(){
+    var valid = true;
+    var InputDefault = document.getElementById("FirstName");
+    var defaultTxt = " Enter First Name";
+    InputDefault.value = defaultTxt;
+    InputDefault.style.color = "#cccccc";
+    InputDefault.style.fontStyle = "italic";
+    InputDefault.onfocus = function() {
+        if (this.value === defaultTxt) {
+          this.value = "";
+          this.style.color = "#000";
+          this.style.fontStyle = "normal";
+          document.getElementById('FirstNameError').innerHTML = "&nbsp;";
+        }
+    }
+    InputDefault.onblur = function() {
+        if (this.value=="") {
+          this.value = defaultTxt;
+          this.style.color = "#cccccc";
+          this.style.fontStyle = "italic";
+          document.getElementById('FirstNameError').innerHTML = ' * You must enter a valid First Name';
+		  valid = false;
+        }
+        return valid;
+    } 
+}
+
+function checkLastName(){
+	var valid = true;
+    var lnameInput = document.getElementById("LastName");
+	if(lnameInput.value==""){
+		document.getElementById('LastNameError').innerHTML = ' * You must enter a valid Last Name';
+		valid = false;
+	}
+    return valid;
+}
+
+// Title Validator: check if an option has been selected.
+function checkTitle(){
+	var valid = true;
+    var titleInput = document.getElementById("Title");
+	if(titleInput.value==0){
+		document.getElementById('TitleError').innerHTML = ' * You must select one option';
+		valid = false;
+	}
+    return valid;
+}
+
+// Number must be entered in the form of a six-digit integer prefixed with the letters ZHA (e.g. ZHA346783 or zha346783).
+function checkHan(){
+	var valid = true;
+    var hanInput = document.getElementById("Han").value;
+    var hanRegEx = /^((ZHA)|(zha))+[0-9]{6}$/;
+	if((hanInput=="") || (!hanRegEx.test(hanInput))){
+		document.getElementById('HanError').innerHTML = ' * You must enter a valid Health Authority Number and Format (e.g. ZHA346783 or zha346783)';
+		valid = false;
+	}
+    return valid;
+}
+
+// Email Validator: check email format.
+function checkEmail(){
+	var valid = true;
+    var emailInput = document.getElementById("Email").value;
+    var emailRegEx = /^\w+@[a-zA-Z0-9_]+?\.[a-zA-Z]{2,}$/;
+	if((emailInput=="") || (!emailRegEx.test( emailInput)) ){
+		document.getElementById('EmailError').innerHTML = ' * You must enter a valid email address';
+		valid = false;
+	}
+    return valid;
+}
+
+// Phone Number Validator : contains only numbers, not letters and has 11 digits (no spaces)
+function checkPhone(){
+    var valid = true;
+    var phoneRegEx = /^\d{11}$/;
+    var phoneInput = document.getElementById("Phone").value;
+	if((phoneInput=="") || (!phoneRegEx.test(phoneInput))){
+		document.getElementById('PhoneError').innerHTML = ' * You must enter a valid telephone number (11 digits)';
+		valid = false;
+	}
+    return valid;
+}
+
+function clearError(){
+	document.getElementById(this.id + 'Error').innerHTML = "&nbsp;";
+	document.getElementById('onclickError').innerHTML = "&nbsp;";
+}
+
+function toolTipDisplay() {           
+    document.getElementById('mark').onmouseover = function() {
+    var toolTip = document.getElementById('tooltip');
+    toolTip.style.display='block';
+    } 	    
+    document.getElementById('mark').onmouseout = function() {
+    var toolTip = document.getElementById('tooltip');
+    toolTip.style.display='none';
+    } 	
+  }	
 
 
-// function getInputValue(name){
-//     var elem = document.contact.name.value;
-//     alert('Invalid Name format', elem);
-     
-//  }
 
- 
--- mcamey01_jv_fma_part1/diabetestool.js
+++ mcamey01_jv_fma_part1/diabetestool.js
@@ -1,7 +1,3 @@
-var elemHook = document.getElementById("mainRisks");
-var factorListParent = document.createElement("ul");
-factorListParent.setAttribute("id", "risks");
-
 // click event triggers binding function
 function bindings() {           
     document.getElementById("submitBtn").onclick = displayResults;
@@ -9,14 +5,12 @@ function bindings() {
 
 // obtained values for array
 function displayResults(){
+    // caluculate Risk and assign labels
     var risks = [];
     risks[0] = calculateRisk("age");
     risks[1] = calculateRisk("bmi");
     risks[2] = calculateRisk("diabetes");
     risks[3] = calculateRisk("diet"); 
-    // order array obtained values
-    // var findMainRisk = risks.sort(function(a,b){return a - b});
-
 
     var risksLabel = [];
     risksLabel[0] = "your Age";
@@ -24,104 +18,19 @@ function displayResults(){
     risksLabel[2] = "your diabetes";
     risksLabel[3] = "your diet";
 
-   
-
-
-
-    // obtained values for comparasion
-    // var age = calculateRisk("age");
-    // var bmi = calculateRisk("bmi");
-    // var diabetes = calculateRisk("diabetes");
-    // var diet = calculateRisk("diet"); 
-
-    // Find Maximum Risk values
-    // var mainRisk = findMainRisk[3];
-    // findMainRisk[2] = 10;
-    // var mainRisktwo = findMainRisk[2];
-
-    // Assign boolean comparasion between the values obtained and the max value
-    // var ageResult = Boolean(age >= mainRisk);
-    // var bmiResult = Boolean(bmi >=  mainRisk);
-    // var diabetesResult = Boolean(diabetes >= mainRisk);
-    // var dietResult = Boolean(diet >= diabetes);
-
+    // iteration to check which risk is grater than 9
     for(i = 0; i < risks.length; i++){
-        mainRisksFactor = Boolean(risks[i] >= 10);
-        if(mainRisksFactor){
-            var factorList = document.createElement("li");
-            var factorListText = document.createTextNode(risksLabel[i]);
-            elemHook.appendChild(factorListParent);
-            factorListParent.appendChild(factorList);
-            factorList.appendChild(factorListText);
-            
-            // var riskFactor = risksLabel[i];
-            // var totalRiskFactors = [];
-            // totalRiskFactors[i] = riskFactor;
-            // document.getElementById("mainRiskOne").innerHTML =  totalRiskFactors[i];
-            // console.log(totalRiskFactors[i])
+        if(risks[i]>9){
+            // console.log(risks[i]);
+            // console.log(risksLabel[i]);
+            // var last = risksLabel[risksLabel.length - 1];
+            var message = document.getElementById("mainRisks").innerHTML;
+            message += ", " + risksLabel[i];
+            document.getElementById("mainRisks").innerHTML = message;
         }
     }
 
-
-  
-
-
-    if(risks.length){
-        lastRisk = document.getElementById("risks").lastElementChild;
-
-        lastRisk.setAttribute("class", "last");
-        document.getElementsByClassName("last").innerHTML = "and" + " " ;
-     
-    }
-
-    // Condition to find the two heightest risks
-    // if(ageResult){
-    //     alert('age')
-    //     document.getElementById("mainRiskOne").innerHTML = "your Age";
-    //     if(bmi >= mainRisktwo){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your BMI";
-    //     }else if(diabetes >= mainRisktwo ){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your family health history";
-    //     }else{
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your diet";
-    //     }
-
-    // }else if(bmiResult){
-    //     alert('bmi')
-    //     document.getElementById("mainRiskOne").innerHTML = "your BMI";
-    //     if(age >= mainRisktwo){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your age";
-    //     }else if(diabetes >= mainRisktwo ){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your family health history";
-    //     }else{
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your diet";
-    //     }
-
-    // }else if(diabetesResult){
-    //     alert('diabetes')
-    //     document.getElementById("mainRiskOne").innerHTML = "your Family Health History";
-    //     if(age >= mainRisktwo){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your age";
-    //     }else if(bmi >= mainRisktwo ){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your BMI";
-    //     }else{
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your diet";
-    //     }
-
-
-    // }else{
-    //     alert('diet')
-    //     document.getElementById("mainRiskOne").innerHTML = "your Diet";
-    //     if(age >= mainRisktwo){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your age";
-    //     }else if(diabetes >= mainRisktwo ){
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your family health history";
-    //     }else{
-    //         document.getElementById("mainRiskTwo").innerHTML = "and your BMI";
-    //     }
-    // }
-
-    // Condition to display the level of risk message
+    // Condition to display the risk level message
     var risksSum = risks.reduce((a, b) => a + b, 0);
     if (risksSum >= 25){
         alert("high");
