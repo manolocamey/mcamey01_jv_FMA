bcb08469d4a79449d69330a977b1216b0f59e891
manolocamey <manolocamey@gmail.com>
2019-08-01 14:28:37 +0100

    fix issue of separating the risk array and display the word and  in the last element of the risk array

--- mcamey01_jv_fma_part1/diabetestool.js
+++ mcamey01_jv_fma_part1/diabetestool.js
@@ -10,25 +10,7 @@ function displayResults(){
     risks[0] = calculateRisk("age");
     risks[1] = calculateRisk("bmi");
     risks[2] = calculateRisk("diabetes");
-    risks[3] = calculateRisk("diet"); 
-
-    var risksLabel = [];
-    risksLabel[0] = "your Age";
-    risksLabel[1] = "your bmi";
-    risksLabel[2] = "your diabetes";
-    risksLabel[3] = "your diet";
-
-    // iteration to check which risk is grater than 9
-    for(i = 0; i < risks.length; i++){
-        if(risks[i]>9){
-            // console.log(risks[i]);
-            // console.log(risksLabel[i]);
-            // var last = risksLabel[risksLabel.length - 1];
-            var message = document.getElementById("mainRisks").innerHTML;
-            message += ", " + risksLabel[i];
-            document.getElementById("mainRisks").innerHTML = message;
-        }
-    }
+    risks[3] = calculateRisk("diet");
 
     // Condition to display the risk level message
     var risksSum = risks.reduce((a, b) => a + b, 0);
@@ -46,6 +28,33 @@ function displayResults(){
         document.getElementById("low").style.display = "block";
         document.getElementById("high").style.display = "none";
         document.getElementById("medium").style.display = "none";
+    }
+    // Setup Risks labels
+    var risksLabel = [];
+    risksLabel[0] = "your Age";
+    risksLabel[1] = "your bmi";
+    risksLabel[2] = "your diabetes";
+    risksLabel[3] = "your diet";
+
+    // Store labels >=10 into a new array to organise them.
+    var highestRisk = [];
+    var separatorTxt =  ', ';
+    var lastSeparatorTxt =  " and ";
+    // iteration to check which risk is grater or equal to ten 
+    for(i = 0; i < risks.length; i++){
+        if(risks[i]>=10){
+            highestRisk.push(risksLabel[i]);
+            document.getElementById("mainRisks").innerHTML = highestRisk.join(separatorTxt);
+        }
+    }
+
+    // Add separator to the display array also puts the word -- and -- to the last element
+    if(highestRisk.length > 1){
+        var findLast = highestRisk.indexOf(highestRisk[highestRisk.length - 1]);
+        var lastLabel = highestRisk[highestRisk.length - 1];
+        var setLast = lastSeparatorTxt.concat(lastLabel);
+        highestRisk.splice(findLast, 1, setLast);
+        document.getElementById("mainRisks").innerHTML = highestRisk.join(separatorTxt);;
     }
 }
 
